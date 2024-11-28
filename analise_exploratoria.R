d5642a321b617b8350226cb574ffce9865409bbe
Vitor Nickhorn Kroeff <vitornkroeff@gmail.com>
2024-11-27 23:57:19 -0300

    analise exploratoria

--- analise_exploratoria.R
+++ analise_exploratoria.R
@@ -9,16 +9,20 @@ dados <- tibble(read.table('base_de_dados.txt', header = T)) # Base do github
 head(dados)
 
 str(dados)
-
 summary(dados)
 
-# Verificação de perda de acompanhamento
-tempos <- c(length(dados$t1),length(dados$t2), length(dados$t3),
-            length(dados$t4), length(dados$t5), length(dados$t6))
+tempos <- c(sum(is.na(dados$t1)), sum(is.na(dados$t2)), sum(is.na(dados$t3)),
+            sum(is.na(dados$t4)), sum(is.na(dados$t5)), sum(is.na(dados$t6)))
 
 tempos #mesmo número de medidas, sem perda de acompanhamento
+
+
+# Tratamento dos dados
+
+dados$sexo <- as.factor(dados$sexo)
 
 # Análise gráfica
 
 
+barplot(table(as.factor(dados$sexo)))
 
