b15d910aec78e602f0d4a292715d91c0434f4317
Vitor Nickhorn Kroeff <vitornkroeff@gmail.com>
2024-11-27 22:35:43 -0300

    comit de teste

--- analise_exploratoria.R
+++ analise_exploratoria.R
@@ -6,8 +6,20 @@ require(ggplot2)
 # Carregamento da base de dados
 
 dados <- tibble(read.table('base_de_dados.txt', header = T)) # Base do github
+head(dados)
 
-View(dados)
+str(dados)
+
+summary(dados)
+
+# Verificação de perda de acompanhamento
 tempos <- c(length(dados$t1),length(dados$t2), length(dados$t3),
             length(dados$t4), length(dados$t5), length(dados$t6))
-tempos
+
+tempos #mesmo número de medidas, sem perda de acompanhamento
+
+# Análise gráfica
+
+
+
+
