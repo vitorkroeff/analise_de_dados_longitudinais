473047fa53db1355675d8c8b12a7cb6cb099cecf
Vitor Nickhorn Kroeff <vitornkroeff@gmail.com>
2024-12-02 19:42:58 -0300

    dados longos

--- analise_exploratoria.R
+++ analise_exploratoria.R
@@ -1,6 +1,7 @@
 # Pacotes
 require(dplyr)
 require(ggplot2)
+require(tidyr)
 
 
 # Carregamento da base de dados
@@ -14,16 +15,26 @@ summary(dados)
 tempos <- c(sum(is.na(dados$t1)), sum(is.na(dados$t2)), sum(is.na(dados$t3)),
             sum(is.na(dados$t4)), sum(is.na(dados$t5)), sum(is.na(dados$t6)))
 
-tempos #mesmo número de medidas, sem perda de acompanhamento
+# correlacão
+
+View(dados)
 
 
 # Tratamento dos dados
 
 dados$sexo <- as.factor(dados$sexo)
 
-# Análise gráfica
+# Análise gráfica Colocar mais coisas
 
 
 barplot(table(as.factor(dados$sexo))) # distribuição por genero
  
 
+# Transformando os dados em fortmato longo
+# Ver se tem um id de paciente
+dados_longos <- dados %>% pivot_longer(
+    cols = starts_with("t"),               
+    names_to = "tempo",                   
+    values_to = "citocina_t",              
+    names_prefix = "t"                    
+)
