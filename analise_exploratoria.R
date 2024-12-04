625752dc5dd0c8a0d7c62532412846d0c172fb21
Vitor Nickhorn Kroeff <vitornkroeff@gmail.com>
2024-12-04 16:31:42 -0300

    Inicio do tratamento na base

--- analise_exploratoria.R
+++ analise_exploratoria.R
@@ -9,41 +9,58 @@ require(ggplot2)
 
 dados_brutos <- tibble(read.table('base_de_dados.txt', header = T)) # Base do github
 
+# Correção na linha 6 da variável n56hpo
+
+dados_brutos$n56hpo[6] <- 1130
+
+# Criação de um id para os pacientes
+
+dados_brutos$id <- 1:length(dados_brutos$idade)
+View(dados_brutos)
 # Seleção das variáveis do estudo
 
-dados <- dados_brutos %>% select(c(sexo, idade,imc, 
+## eurob e euros medem risco de evento cardiáco, usar apenas uma delas.
+
+dados <- dados_brutos %>% select(c(id,sexo, idade,imc, 
                                    nyha, has, iap, ai,
                                    eurob, euroes, fc, creat,
-                                   t1, t2, t3, t4, t5, t6))
+                                   t1, t2, t3, t4, t5, t6,
+                                   grupo)) 
 
-head(dados)
 
 # Tratamento dos dados categoricos
-dados$sexo <- as.factor(ifelse(dados$sexo <2, 'M', 'F' ))
-dados$nyha <- as.factor(dados$sexo)
+dados$sexo <- as.factor(ifelse(dados$sexo== 1, 'M', 'F' ))
+dados$nyha <- as.factor(dados$nyha)
+dados$has <- as.factor(dados$has)
+dados$iap <- as.factor(dados$iap)
+dados$ai <- as.factor(dados$ai)
+dados$grupo <- as.factor(dados$grupo)
 
-factor()
+str(dados)
 summary(dados)
 
+# Primeiras linhas da base
+head(dados)
 
+View(dados)
 
-# Análise gráfica Colocar mais coisas
+# Análise gráfica
 
-
-barplot(dados$sexo)
-# distribuição por genero
+## Evolução por gênero
+View(dados)
 
 
  
 
-# Transformando os dados em fortmato longo # ERRADO PRECISA PASSAR VARIOS PARA LONGO
+# Transformando os dados em fortmato longo
 # Ver se tem um id de paciente
 dados_longos <- dados %>% pivot_longer(
     cols = starts_with("t"),               
-    names_to = "tempo",                   
+    names_to = "observacao",                   
     values_to = "citocina_t",              
     names_prefix = "t"                    
 )
 
 
-table(dados_longos$tempo)
+# Não há perda de acompanhamento no estudo
+barplot(table(dados_longos$id))
